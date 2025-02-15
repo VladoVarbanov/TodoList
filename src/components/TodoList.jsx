34a81800461f2b1d8c2efe07e19976cab0f430c8
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2025-02-15 13:12:45 +0200

    Finishing the app

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import TodoCard from "./TodoCard.jsx";
 
-export default function TodoList() {
-  return <div>TodoList</div>;
+export default function TodoList(props) {
+  const { todos } = props;
+
+  return (
+    <ul className="main">
+      {todos.map((todo, todoIndex) => {
+        return (
+          <TodoCard {...props} key={todoIndex} index={todoIndex}>
+            <p>{todo}</p>
+          </TodoCard>
+        );
+      })}
+    </ul>
+  );
 }
